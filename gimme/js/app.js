7bfc05af0170569d11ef8e7d526eefa6239034e1
AngryTrumpet <maxwell.barassi@sydstu.catholic.edu.au>
2016-05-09 16:57:29 +1000

    working base program
    Working ADD Page
    and blank HOME page

--- gimme/js/app.js
+++ gimme/js/app.js
@@ -1,7 +1,4 @@
-//CRUD using AngularJS and ngStorage
-//Created by Peter Cox
-//11 April 2015
-//Copyright  2014  PCSquared.com.au
+//Created by Max Barassi
 
 			// creates the Angular modular "app"
 			// NOTE: we inject into this module the ngStorage object  AND bsTable object
@@ -18,9 +15,8 @@
 					$scope.storage = $localStorage.$default({
     						datasource: [{
 
-
-										}]
-					});  // end of creating localStorage 'datasource'
+								}]
+				});  // end of creating localStorage 'datasource'
 
 
 
@@ -36,127 +32,27 @@
 				// We add to the localstorage via push  -> $scope.storage.datasource.push({
 					$scope.addGift = function () {
 							$scope.storage.datasource.push({
-									giftname:			$scope.giftname,
+									giftname:			  $scope.giftname,
 									giftbuyer: 			$scope.giftbuyer,
-									giftcost: 	$scope.giftcost,
-									giftpriority:			$scope.giftpriority,
-									giftstore: 		$scope.giftstore,
-									giftdate: 	$scope.giftdate,
+									giftcost: 	    $scope.giftcost,
+									giftpriority:	 	$scope.giftpriority,
+									giftstore: 		  $scope.giftstore,
+									giftdate: 	    $scope.giftdate,
 									giftnotes: 			$scope.giftnotes,
 
 								});
 
 								// Clear form input fields after push captured data into localstorage
-								$scope.giftname 		= 	"";
-								$scope.giftbuyer 		= 	"";
-								$scope.giftcost 	= 	"";
-								$scope.giftpriority		=	"";
-								$scope.giftstore 	= 	"";
-								$scope.giftdate = 	"";
-								$scope.giftnotes 		= 	"";
+								$scope.giftname 	  	= 	"";
+								$scope.giftbuyer 	  	= 	"";
+								$scope.giftcost 	    =   "";
+								$scope.giftpriority   =  	"";
+								$scope.giftstore 	    =	  "";
+								$scope.giftdate       =   "";
+								$scope.giftnotes 		  = 	"";
 
 
-					}; //end addPerson
-
-
-
-					// Used in PAGE :  Listview Edit & delete   AND  TableVie Edit & delete
-					$scope.saveEdit = function (index) {
-								$scope.storage.datasource[index].giftname 		= 		$scope.giftname;
-								$scope.storage.datasource[index].giftbuyer 			=		$scope.giftbuyer;
-								$scope.storage.datasource[index].giftcost 	=		$scope.giftcost;
-								$scope.storage.datasource[index].giftpriority 			=		$scope.giftpriority;
-								$scope.storage.datasource[index].giftstore 		=		$scope.giftstore ;
-								$scope.storage.datasource[index].giftdate 	=		$scope.giftdate ;
-								$scope.storage.datasource[index].giftnotes 			=		$scope.giftnotes ;
-
-					 };// end saveEdit
-
-
-					 // Used in PAGE :  Listview Edit & delete   AND  TableVie Edit & delete
-					$scope.deleteGift = function (index) {
-								$scope.index 		= 		index;
-								$scope.giftname 		= 		$scope.storage.datasource[index].giftname ;
-								$scope.giftbuyer 		= 		$scope.storage.datasource[index].giftbuyer ;
-								$scope.giftcost 	= 		$scope.storage.datasource[index].giftcost ;
-								$scope.giftpriority 		= 		$scope.storage.datasource[index].giftpriority ;
-								$scope.giftstore 	= 		$scope.storage.datasource[index].giftstore
-								$scope.giftdate = 		$scope.storage.datasource[index].giftdate ; ;
-								$scope.giftnotes 		= 		$scope.storage.datasource[index].giftnotes ;
-
-					};
-
-
-					// Used in PAGE :  Listview Edit & delete   AND  TableVie Edit & delete
-					$scope.deleteGiftYes = function (index) {
-								$scope.storage.datasource.splice(index, 1);
-					}; // end deleteGiftYes
-
-					$scope.deleteGift_bstable = function (item) {
-						// indexOf: this identifies the index of the object item that has been passed
-						var index = $scope.storage.datasource.indexOf(item);
-  						$scope.storage.datasource.splice(index, 1);
-					}; // end deleteGiftYes
-
-
-
-					// Used in PAGE :  Filter
-					 $scope.editGift = function (index) {
-								$scope.index 		= 		index;
-								$scope.giftname 		= 		$scope.storage.datasource[index].giftname ;
-								$scope.giftbuyer			= 		$scope.storage.datasource[index].giftbuyer ;
-								$scope.giftcost 	= 		$scope.storage.datasource[index].giftcost ;
-								$scope.giftpriority 		= 		$scope.storage.datasource[index].giftpriority ;
-								$scope.giftstore 	= 		$scope.storage.datasource[index].giftstore ;
-								$scope.giftdate = 		$scope.storage.datasource[index].giftdate ;
-								$scope.giftnotes 		= 		$scope.storage.datasource[index].giftnotes ;
-
-					 };	// end editGift
-
-					// Used in PAGE :  Filter
-					// In the html filter page the drop-down selection value is passed here as "selection"
-					// The $scope.orderGifts is set depending on what you selected eg 'giftstore'
-					// The $scope.orderGifts can now be access back in the html page to set the filter parameters
-					// eg <tr ng-repeat="item in Storage = (storage.datasource | filter:searchQuery) | orderBy:orderGifts ">
-					$scope.orderBy = function(selection) {
-						if(selection == "giftbuyer"){
-							$scope.orderGifts = "giftbuyer";
-						}
-						if(selection == "giftstore"){
-							$scope.orderGifts = "giftstore";
-						}
-						if(selection == "giftdate"){
-							$scope.orderGifts = "giftdate";
-						}
-						if(selection == "giftnotes"){
-							$scope.orderGifts = "giftnotes";
-						}
-
-				  	};	// end orderBy
-
-
-
-					// Used in PAGE :  Reports
-					$scope.showTotalgiftnotes = function() {
-								//assign datasourceto an temp array called data
-								data = $scope.storage.datasource;
-								//declare totalgiftnotes
-								var totalgiftnotes = 0;
-								// loop over temp array and add giftnotes for each item in array eg data[i].proce
-								for(var i = 0; i < data.length; i++) {
-									totalgiftnotes	+=  Number(data[i].giftnotes)
-								}
-								numberofitems = i;
-								$scope.reportgiftnotes = "There were " + numberofitems + " Gifts.  The total giftnotes of all Gifts is: $" + totalgiftnotes ;
-					}// end showTotalgiftnotes
-
-
-
-					// This deletes or clears ALL records in your localstorage
-					// Be careful when using this
-					$scope.clearAll = function(){
-							$localStorage.$reset();
-					}	// end clearALL
+					}; //end addGift
 
 	});// end 'giftController' controller
 
